3609cdb6b353c088690ea74e3df12f1955663287
Davidson Campos <davidsonc@gmail.com>
2025-06-04 07:16:25 -0300

    feat: implement dashboard page with session handling and sign out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,29 @@
-const DashboardPage = () => {
-  return <h1>Dashboard</h1>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {session ? (
+        <div>
+          <p>Bem-vindo, {session?.user?.name}!</p>
+          <p>Email: {session?.user?.email}</p>
+          <SignOutButton />
+        </div>
+      ) : (
+        <p>Você não está autenticado.</p>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div className="dark flex h-screen w-full items-center justify-center">
-      <Button>Button</Button>
+      <div className="flex flex-col items-center gap-4">
+        <h1>HOMEPAGE</h1>
+        <Button asChild>
+          <Link href="/authentication">Ir para Login</Link>
+        </Button>
+      </div>
     </div>
   );
 };
